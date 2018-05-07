6b9b7d577c426d90523014fb74356797805dbbdf
Ignacio Esmite <iesmite@gmail.com>
2018-05-07 00:30:34 -0300

    covereye html report

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # covereye
+
+The aim of this project is to define a model of the application under test. After a model is defined it is posible to mitigate risks due to lack of testing in different areas.
+
+# usage
+
+```
+npm install covereye
+covereye generate pathToYamlFile 
+```
+
+The generated file will be output.html
---
+++ index.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+const util = require('util');
+const handlebars = require('handlebars');
+const fs = require('fs');
+
+const matrix = require('./lib/matrix');
+const package = require('./package.json');
+
+program
+  .version(package.version);
+
+program
+  .command('generate <yamlFile>')
+  .description('Generate the html for coverage matrix')
+  .action(function (yamlFile, cmd, options) {
+    try {
+      const acc = new matrix(yamlFile);
+      const fullACC = acc.getFullMatrix();
+      const template = fs.readFileSync('lib/templates/table.hbs', 'utf8');
+      const compiledTemplate = handlebars.compile(template);
+      fs.writeFileSync('output.html', compiledTemplate({body: fullACC, attributes: acc.getMatrix().attributes}), 'utf8');
+      console.log('Covereye report generate at output.html. Type `open output.html` to open it.');
+    } catch (e) {
+      console.log(e);
+    }
+  });
+
+program.parse(process.argv);
+
+if (!program.args.length) program.help();
+
